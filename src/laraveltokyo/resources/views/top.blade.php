bfcef095fd41b356ec5ac07457ca9b3e88bdb266
m2gen <ymslash78@gmail.com>
2023-11-30 23:38:24 +0900

    delete bafixes

--- src/laraveltokyo/resources/views/top.blade.php
+++ src/laraveltokyo/resources/views/top.blade.php
@@ -8,7 +8,6 @@
             background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('images/背景画像候補１.jpg');
             background-size: cover;
 	    background-position: 50% 25%;
-	    background-attachment: fixed;
             height: 400px;
         }
 
